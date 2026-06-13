49f82a09ee1d7e0468c51171fdda3253b61c9e76
Ritik Yadav <ritik783026yadav@gmail.com>
2026-06-13 19:23:44 +0530

    Add start script for render deployment

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -4,8 +4,13 @@ import cookieParser from "cookie-parser"
 
 const app = express()
 
+// app.use(cors({
+//     origin: "http://localhost:5173",
+//     credentials: true
+// }))
+
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: process.env.CORS_ORIGIN,
     credentials: true
 }))
 
